75e473c4bf0a6e2900dc4e63b9d1a994ecb79c7a
Sam Greider <sam.greider7@gmail.com>
2020-05-04 12:44:04 -0400

    fixed margins with the project cards

--- src/components/project.js
+++ src/components/project.js
@@ -23,7 +23,7 @@ export default Project
 const Card = styled.section`
   text-align: center;
   width: 80%;
-  margin: 0;
+  margin: 0 auto;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 
   :hover {
